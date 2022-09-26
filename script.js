9fcedc654af90d8a35d605e237b144f410e7635e
Omar Moreno <omarmorenoramir@gmailcom>
2022-09-26 11:11:26 -0500

    html actualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("Saoko papi saoko");
+    document.getElementById("generation").style.color = "grey";
+    
+    function colorTitulo(){
+      console.log("Pulso e boton de Generation")
+      document.getElementById("generation").style.color = "blue";
+    }
+    
